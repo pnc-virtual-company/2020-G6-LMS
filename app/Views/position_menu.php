24e1160b6590250d1e32732305d4034ce9e7f54f
simdoe <simdoem99@gmail.com>
2020-07-10 08:00:36 +0700

    #4 design  mock up.

---
+++ app/Views/position_menu.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12"></div>
+        <div class="col-8">
+            <div class="text-right">
+                <a href="index" class="btn btn-warning btn-sm text-white font-weight-bolder" data-toggle="modal" data-target="#createPizza">
+					<i class="material-icons float-left" data-toggle="tooltip" title="Add Pizza!" data-placement="left">Create</i>&nbsp;Add
+				</a>
+            </div>
+        </div>
+    </div>
+</div>
