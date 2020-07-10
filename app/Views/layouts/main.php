d34a36fe1299c693b99b40326a6fd0554a561383
simdoe <simdoem99@gmail.com>
2020-07-10 23:45:09 +0700

    #8 position view: create update and delete

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Peperoni App</title>
+    <title>LMS APP</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
